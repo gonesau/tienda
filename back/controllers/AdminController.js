745017e030b4700d3efc97eb8a875b9556da8f05
gonesau <gonesau20@gmail.com>
2025-07-30 15:50:10 -0600

    Inicio de sesión y generar tokens en backend

--- back/controllers/AdminController.js
+++ back/controllers/AdminController.js
@@ -1,6 +1,7 @@
 'use strict';
 var Admin = require('../models/admin');
 var bcrypt = require('bcrypt-nodejs');
+var jwt = require('../helpers/jwt');
 
 const registro_admin = async (req, res) => {
     var data = req.body;
@@ -26,6 +27,29 @@ const registro_admin = async (req, res) => {
     }
 }
 
+const login_admin = async (req, res) => {
+  var data = req.body;
+  var admin_arr = [];
+  admin_arr = await Admin.find({ email: data.email });
+  if (admin_arr.length == 0) {
+    return res.status(404).send({ message: "El Admin no existe", data: undefined });
+  } else {
+    //Login
+    let user = admin_arr[0];
+    bcrypt.compare(data.password, user.password, async function (error, check) {
+      if (check) {
+        return res.status(200).send({ 
+          data: user,
+          token: jwt.createToken(user)
+        });
+      }else{
+        res.status(400).send({message: "Error al iniciar sesión, verifique sus credenciales",data: undefined,}); 
+      }
+    });
+  }
+};
+
 module.exports = {
-    registro_admin
+    registro_admin,
+    login_admin
 };
--- back/routes/admin.js
+++ back/routes/admin.js
@@ -7,5 +7,6 @@ const admin = require('../models/admin');
 var api = express.Router();
 
 api.post('/registro_admin', adminController.registro_admin);
+api.post('/login_admin', adminController.login_admin);
 
 module.exports = api;
