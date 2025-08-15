ffaf68f69bbc60d0973f024d0085d8d41937f5aa
gonesau <gonesau20@gmail.com>
2025-08-15 15:37:50 -0600

    Inicio de sesión en angular, panel administrador

--- admin/src/app/components/login/login.component.ts
+++ admin/src/app/components/login/login.component.ts
@@ -1,20 +1,80 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AdminService } from '../../services/admin.service';
 
 declare var jQuery: any;
 declare var $: any;
+declare var iziToast: any;
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  public user: any = {};
+  public usuario: any = {};
+  public token: any = '';
 
-  constructor() { 
-    
+  constructor(
+    private _adminService: AdminService,
+    private _router: Router
+  ) {
+    this.token = this._adminService.getToken();
   }
 
   ngOnInit(): void {
+    if (this.token) {
+      this._router.navigate(['/']);
+    } else {
+      // Se mantiene en el componente de login
+    }
   }
 
+  login(loginForm) {
+    if (loginForm.valid) {
+      console.log(this.user);
+
+      let data = {
+        email: this.user.email,
+        password: this.user.password,
+      };
+
+      this._adminService.login_admin(data).subscribe(
+        (response) => {
+          if (response.data == undefined) {
+            iziToast.show({
+              title: 'Error',
+              titleColor: '#FF0000',
+              color: '#FFF',
+              class: 'text-danger',
+              position: 'topRight',
+              message: response.message,
+            });
+          } else {
+            this.usuario = response.data;
+            localStorage.setItem('token', response.token);
+            localStorage.setItem('_id', response.data._id);
+            
+            this._router.navigate(['/']);
+
+
+          }
+          console.log(response);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    } else {
+      iziToast.show({
+        title: 'Error',
+        titleColor: '#FF0000',
+        color: '#FFF',
+        class: 'text-danger',
+        position: 'topRight',
+        message: 'Los datos del formulario no son válidos',
+      });
+    }
+  }
 }
--- back/controllers/AdminController.js
+++ back/controllers/AdminController.js
@@ -32,7 +32,7 @@ const login_admin = async (req, res) => {
   var admin_arr = [];
   admin_arr = await Admin.find({ email: data.email });
   if (admin_arr.length == 0) {
-    return res.status(404).send({ message: "El Admin no existe", data: undefined });
+    return res.status(200).send({ message: "El Admin no existe", data: undefined });
   } else {
     //Login
     let user = admin_arr[0];
@@ -43,7 +43,7 @@ const login_admin = async (req, res) => {
           token: jwt.createToken(user)
         });
       }else{
-        res.status(400).send({message: "Error al iniciar sesión, verifique sus credenciales",data: undefined,}); 
+        res.status(200).send({message: "Error al iniciar sesión, verifique sus credenciales",data: undefined,}); 
       }
     });
   }
