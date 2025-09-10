1514e41b831c97fdd8624e1676bac5f5e0fe91be
gonesau <gonesau20@gmail.com>
2025-09-10 08:36:04 -0600

    Registro de producto y subida de imagen a backend | Done

--- back/controllers/ProductoController.js
+++ back/controllers/ProductoController.js
@@ -6,9 +6,14 @@ const registro_producto_admin = async function(req, res){
     if(req.user){
         if(req.user.role == 'admin'){
             let data = req.body;
-            console.log('Body:', data);
-            console.log('Files:', req.files); 
-            res.status(200).send({ message: 'Producto recibido', data });
+            var img_path = req.files.portada.path;
+            var name = img_path.split('\\');
+            var portada_name = name[2];
+            data.portada = portada_name;
+            data.slug = data.titulo.toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g, '');
+            let reg = await Product.create(data);
+            res.status(200).send({data: reg});
+
         } else {
             res.status(500).send({message: 'NoAccess'});
         }
