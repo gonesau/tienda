3f65e828547fc137569c7f6cfa1fe69158590371
gonesau <gonesau20@gmail.com>
2025-09-12 22:47:53 -0600

    Listar, filtrar y paginar productos

--- admin/src/app/components/productos/index-producto/index-producto.component.ts
+++ admin/src/app/components/productos/index-producto/index-producto.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Global } from 'src/app/services/global';
 import { ProductoService } from 'src/app/services/producto.service';
+declare var iziToast: any;
 
 @Component({
   selector: 'app-index-producto',
@@ -14,6 +15,8 @@ export class IndexProductoComponent implements OnInit {
   public token;
   public productos: Array<any> = [];
   public url;
+  public page = 1;
+  public pageSize = 10;
 
   constructor(
     private _productoService: ProductoService
@@ -23,7 +26,11 @@ export class IndexProductoComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this._productoService.listar_productos_admin(this.filtro, this.token).subscribe(
+    this.init_data();
+  }
+
+  init_data(){
+        this._productoService.listar_productos_admin(this.filtro, this.token).subscribe(
       response => {
         console.log(response);
         this.productos = response.data;
@@ -33,6 +40,35 @@ export class IndexProductoComponent implements OnInit {
         console.error(error);
       }
     );
+  }
+
+  filtrar() {
+    if (this.filtro) {
+          this._productoService.listar_productos_admin(this.filtro, this.token).subscribe(
+      response => {
+        console.log(response);
+        this.productos = response.data;
+        this.load_data = false;
+      },
+      error => {
+        console.error(error);
+      }
+    );
+    } else{
+              iziToast.show({
+          title: 'Error',
+          titleColor: '#FF0000',
+          color: '#FFF',
+          class: 'text-danger',
+          position: 'topRight',
+          message: 'Ingrese un filtro válido',
+      });
+    }
+  }
+
+  resetear() {
+    this.filtro = '';
+    this.init_data();
   }
 
 }
