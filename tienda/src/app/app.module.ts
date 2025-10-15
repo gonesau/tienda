679496cd88201fd26871d3f4486c1b62a5f0856b
gonesau <gonesau20@gmail.com>
2025-10-14 19:37:24 -0600

    Login de tienda min 10

--- tienda/src/app/app.module.ts
+++ tienda/src/app/app.module.ts
@@ -11,9 +11,10 @@ import { AppComponent } from './app.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { NavComponent } from './components/nav/nav.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
-  declarations: [AppComponent, InicioComponent, NavComponent, FooterComponent],
+  declarations: [AppComponent, InicioComponent, NavComponent, FooterComponent, LoginComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule, routing],
   providers: [],
   bootstrap: [AppComponent],
---
+++ tienda/src/app/components/login/login.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  public user : any = {};
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  login(loginForm) {
+    if (loginForm.valid) {
+
+    } else {
+      
+    }
+  }
+
+}
